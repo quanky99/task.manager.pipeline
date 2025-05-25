66b31b90345a766532223198caf74413cc7d9a66
Maverick <quanky99@gmail.com>
2025-05-26 00:50:09 +1000

    Fix no permission to push erorr

--- scripts/update_index.sh
+++ scripts/update_index.sh
@@ -14,8 +14,11 @@ update_pre "todo-tasks" "$todo_output" index.html
 update_pre "done-tasks" "$done_output" index.html
 update_pre "test-results" "$test_output" index.html
 
-git config user.email "ci@github.com"
-git config user.name "github-actions"
+git config --global user.email "ci@github.com"
+git config --global user.name "GitHub Actions"
+
 git add index.html
 git commit -m "Update index.html with latest task and test results"
-git push
+
+# Use GitHub token for authentication
+git push https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git HEAD:${GITHUB_REF##*/}
